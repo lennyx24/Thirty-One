10f30250681583cf01a73f07e97351125093a200
lennyx24 <lenny.florin@gmail.com>
2025-10-31 12:41:34 +0100

    Added Invisible Card

--- src/main/scala/de/htwg/model/Card.scala
+++ src/main/scala/de/htwg/model/Card.scala
@@ -9,12 +9,4 @@ case class Card(symbol: Char, value: String, size: Int = 10) {
     (cells + "\n") * (size/2) +
     bar + "\n"
   }
-
-  //def invBar(size: Int): String = " " * size + "  "
-  //def invCells(size: Int): String = " " * size + "  "
-  //def invCard(size: Int): String = {
-  //  invBar(size) + "\n" +
-  //    (invCells(size) + "\n") * (size / 2) +
-  //    invBar(size) + "\n"
-  //}
 }
