19d48f03298fb7458f971190ba26df3c03e3dcf1
lennyx24 <lenny.florin@gmail.com>
2025-11-07 12:46:39 +0100

    Updated Tests

--- src/test/scala/de/htwg/se/thirtyone/model/InvCardSpec.scala
+++ src/test/scala/de/htwg/se/thirtyone/model/InvCardSpec.scala
@@ -7,15 +7,15 @@ class InvCardSpec extends AnyWordSpec{
   "Card" should {
 
     "have scalable cells" in {
-      val c1 = InvisibleCard(1).invCell should be("   ")
-      val c2 = InvisibleCard(2).invCell should be("    ")
-      val c3 = InvisibleCard(5).invCell should be("       ")
+      val c1 = InvisibleCard(1).invCell should be("    ")
+      val c2 = InvisibleCard(2).invCell should be("     ")
+      val c3 = InvisibleCard(5).invCell should be("        ")
     }
 
     "have a scalable size" in {
-      val c1 = InvisibleCard(1).invCard should be("   \n   \n")
-      val c2 = InvisibleCard(2).invCard should be("    \n    \n    \n")
-      val c3 = InvisibleCard(5).invCard should be("       \n       \n       \n       \n")
+      val c1 = InvisibleCard(1).invCard should be("    \n    \n")
+      val c2 = InvisibleCard(2).invCard should be("     \n     \n     \n")
+      val c3 = InvisibleCard(5).invCard should be("        \n        \n        \n        \n")
     }
   }
 }
