1ad9c84f99e05fbf354f09fca938fb4fc6411600
lennyx24 <lenny.florin@gmail.com>
2025-11-07 11:03:53 +0100

    Updated Tests

--- src/test/scala/de/htwg/se/thirtyone/model/DeckSpec.scala
+++ src/test/scala/de/htwg/se/thirtyone/model/DeckSpec.scala
@@ -1,5 +1,5 @@
 // scala
-package de.htwg.model
+package de.htwg.se.thirtyone.model
 
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
--- src/test/scala/de/htwg/se/thirtyone/model/TableSpec.scala
+++ src/test/scala/de/htwg/se/thirtyone/model/TableSpec.scala
@@ -1,4 +1,4 @@
-package de.htwg.model
+package de.htwg.se.thirtyone.model
 
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
