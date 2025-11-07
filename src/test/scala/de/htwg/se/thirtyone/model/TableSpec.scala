93b984c72fa40592355258730a075c8650609b69
Daniel Sauer <Daniel.Sauer@htwg-konstanz.de>
2025-11-07 19:05:39 +0100

    TableSpec test updated

--- src/test/scala/de/htwg/se/thirtyone/model/TableSpec.scala
+++ src/test/scala/de/htwg/se/thirtyone/model/TableSpec.scala
@@ -5,18 +5,32 @@ import org.scalatest.matchers.should.Matchers._
 
 class TableSpec extends AnyWordSpec{
   "Table" should {
-    "have the format" in {
-      val t1 = Table(1,1) should be (Table(1,1))
-      val t2 = Table(2,2) should be (Table(2,2))
-      val t3 = Table() should be (Table(3,9))
-    }
     "be editable" in {
-      val t1 = Table(1, 1)
-      t1.set(0,0, Card('d', "4")) should be (Vector(Vector(Some(Card('d',"4",10)))))
-      val t2 = Table(2, 2)
-      t2.set(1, 1, Card('d', "4")) should be(Vector(Vector(None, None), Vector(None, Some(Card('d',"4",10)))))
-      val t3 = Table(3, 3)
-      t3.set(1, 1, Card('d', "4")) should be(Vector(Vector(None, None, None), Vector(None, Some(Card('d',"4",10)), None), Vector(None, None, None)))
+      val tab: Table = Table()
+      val h10: Card = Card('h', "10")
+      val d4: Card = Card('d', "4")
+      val s7: Card = Card('s', "7")
+
+      val newTab1 = tab.set(0,1, h10)
+      val s1 = newTab1.toString
+      s1 should include ("h10")
+      s1 should include ("+----------")
+      s1 should not include ("d4")
+      s1 should not include ("s7")
+
+      val newTab2 = newTab1.set(0,2, d4)
+      val s2 = newTab2.toString
+      s2 should include ("h10")
+      s2 should include ("d4")
+      s2 should include ("+----------")
+      s2 should not include ("s7")
+
+      val newTab3 = newTab2.set(1,3,s7)
+      val s3 = newTab3.toString
+      s3 should include ("h10")
+      s3 should include ("d4")
+      s3 should include ("s7")
+      s3 should include ("+----------")
     }
   }
 }
