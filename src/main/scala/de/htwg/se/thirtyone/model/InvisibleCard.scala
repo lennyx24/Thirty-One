ff9120a815a4ac119253af93ce58e20dd80fb7d9
lennyx24 <lenny.florin@gmail.com>
2025-11-07 12:23:54 +0100

    Added working Table

--- src/main/scala/de/htwg/se/thirtyone/model/InvisibleCard.scala
+++ src/main/scala/de/htwg/se/thirtyone/model/InvisibleCard.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.thirtyone.model
 
 case class InvisibleCard(size: Int = 10) {
-  def invCell: String = " " * size + "  "
+  def invCell: String = " " * size + "   "
   def invCard: String = {
     invCell + "\n" +
     (invCell + "\n") * (size / 2) +
--- src/main/scala/de/htwg/se/thirtyone/model/Table.scala
+++ src/main/scala/de/htwg/se/thirtyone/model/Table.scala
@@ -1,24 +1,51 @@
 package de.htwg.se.thirtyone.model
 
-case class Table(height: Int = 3, width: Int = 9) {
-  val table: Vector[Vector[Option[Card]]] = Vector.fill(height, width)(None)
-  
-  def set(height: Int, width: Int, card: Card): Vector[Vector[Option[Card]]] = {
-    val row = table(height)
-    val newRow = row.updated(width, Some(card))
-    val newTbl = table.updated(height, newRow)
-    newTbl
+case class Table(
+  height: Int = 3,
+  width: Int = 9,
+  grid: Vector[Vector[Option[Card]]] = Vector.fill(3, 9)(Option.empty[Card])
+) {
+  def set(h: Int, w: Int, card: Card): Table = {
+    val row = grid(h)
+    val newRow = row.updated(w, Some(card))
+    val newTbl = grid.updated(h, newRow)
+    copy(grid = newTbl)
   }
 
   override def toString: String = {
     val invisibleCard: InvisibleCard = InvisibleCard()
     val output: StringBuilder = new StringBuilder()
-    table.foreach { row =>
-      row.foreach {
-        case Some(s) => output ++= s.cardSize
-        case None => output ++= invisibleCard.invCard
+    grid.foreach { row =>
+      val barString: StringBuilder = new StringBuilder()
+      val topCellString: StringBuilder = new StringBuilder()
+      val cellString: StringBuilder = new StringBuilder()
+
+      var sizeCard: Int = 0
+
+      for (i <- row.indices) {
+        row(i) match {
+          case Some(s) => {
+            barString ++= s.bar
+            topCellString ++= s.topCell
+            cellString ++= s.cells
+
+            sizeCard = s.size
+          }
+          case None => {
+            barString ++= invisibleCard.invCell
+            topCellString ++= invisibleCard.invCell
+            cellString ++= invisibleCard.invCell
+          }
+        }
       }
-      output ++= "\n"
+      barString ++= "\n"
+      topCellString ++= "\n"
+      cellString ++= "\n"
+
+      output ++= barString
+      output ++= topCellString
+      for (i <- 1 until (sizeCard/2)) output ++= cellString
+      output ++= barString
     }
     output.result()
   }
