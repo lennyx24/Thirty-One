c0d4e40d18e447b3b93b9764e35b810ecd78a345
Daniel Sauer <Daniel.Sauer@htwg-konstanz.de>
2025-11-14 11:27:49 +0100

    GameManager Test updated

--- src/test/scala/de/htwg/se/thirtyone/controller/GameManagerSpec.scala
+++ src/test/scala/de/htwg/se/thirtyone/controller/GameManagerSpec.scala
@@ -1,5 +1,21 @@
 package de.htwg.se.thirtyone.controller
 
-class GameManagerSpec {
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers.*
 
+class GameManagerSpec extends AnyWordSpec {
+  "GameManager" should{
+    "be able to pass" in {
+      val playersTurn = 1
+      GameManager.pass(playersTurn) should be ("Spieler 1 passt diese Runde\n")
+      val playersTurn2 = 3
+      GameManager.pass(playersTurn2) should be("Spieler 3 passt diese Runde\n")
+    }
+    "be able to knock" in{
+      val playersTurn = 1
+      GameManager.knock(playersTurn) should be("Spieler 1 klopft diese Runde\n")
+      val playersTurn2 = 3
+      GameManager.knock(playersTurn2) should be("Spieler 3 klopft diese Runde\n")
+    }
+  }
 }
