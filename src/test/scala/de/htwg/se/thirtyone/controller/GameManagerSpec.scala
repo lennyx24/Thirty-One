c9be300669b3818b070a371302b164fd34230140
Daniel Sauer <Daniel.Sauer@htwg-konstanz.de>
2025-11-14 12:15:21 +0100

    GameManager Test added swapALl

--- src/test/scala/de/htwg/se/thirtyone/controller/GameManagerSpec.scala
+++ src/test/scala/de/htwg/se/thirtyone/controller/GameManagerSpec.scala
@@ -3,6 +3,8 @@ package de.htwg.se.thirtyone.controller
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers.*
 
+import de.htwg.se.thirtyone.model._
+
 class GameManagerSpec extends AnyWordSpec {
   "GameManager" should{
     "be able to pass" in {
@@ -16,6 +18,19 @@ class GameManagerSpec extends AnyWordSpec {
       GameManager.knock(playersTurn) should be("Spieler 1 klopft diese Runde\n")
       val playersTurn2 = 3
       GameManager.knock(playersTurn2) should be("Spieler 3 klopft diese Runde\n")
+    }
+    "be able to swapAll" in {
+      val playerTurn = 1
+      var tab: Table = Table()
+      tab = tab.set((0, 1), Card('♦', "10", 10))
+      tab = tab.set((0, 2), Card('♦', "10", 10))
+      tab = tab.set((0, 3), Card('♦', "10", 10))
+      tab = tab.set((1, 3), Card('♦', "9", 10))
+      tab = tab.set((1, 4), Card('♦', "9", 10))
+      tab = tab.set((1, 5), Card('♦', "9", 10))
+      tab = GameManager.swapAll(playerTurn, tab)
+      tab.get((0,1)) should be (Card('♦', "9", 10))
+      tab.get((1,3)) should be (Card('♦', "10", 10))
     }
   }
 }
