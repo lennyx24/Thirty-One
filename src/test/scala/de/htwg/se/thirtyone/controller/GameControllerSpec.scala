b54d92d9c199c10ff141f10c535220e05e1b5812
Daniel Sauer <Daniel.Sauer@htwg-konstanz.de>
2025-11-21 10:56:24 +0100

    GameMananagerSpec test renamed to GameControllerSpec

--- src/test/scala/de/htwg/se/thirtyone/controller/GameControllerSpec.scala
+++ src/test/scala/de/htwg/se/thirtyone/controller/GameControllerSpec.scala
@@ -5,16 +5,16 @@
 
  import de.htwg.se.thirtyone.model._
 
- class GameManagerSpec extends AnyWordSpec {
-   "GameManager" should {
+ class GameControllerSpec extends AnyWordSpec {
+   "GameController" should {
      "be able to calculate index" in {
        val dummyState = GameState(Table(), 3, 1, Deck(), false, Nil)
        val indexToGive = "1"
-       GameManager(dummyState).calculateIndex(indexToGive) should be (0)
+       GameController(dummyState).calculateIndex(indexToGive) should be (0)
        val indexToGive2 = "2"
-       GameManager(dummyState).calculateIndex(indexToGive2) should be(1)
+       GameController(dummyState).calculateIndex(indexToGive2) should be(1)
        val indexToGive3 = "3"
-       GameManager(dummyState).calculateIndex(indexToGive3) should be(2)
+       GameController(dummyState).calculateIndex(indexToGive3) should be(2)
      }
    }
  }
