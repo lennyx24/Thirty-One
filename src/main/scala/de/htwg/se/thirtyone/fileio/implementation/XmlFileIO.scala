005f6d4b3d45abc571471aee6815446721b7e5c9
Daniel Sauer <Daniel.Sauer@htwg-konstanz.de>
2026-01-16 14:31:53 +0100

    fixed saving from fileIO for different os

--- src/main/scala/de/htwg/se/thirtyone/fileio/implementation/XmlFileIO.scala
+++ src/main/scala/de/htwg/se/thirtyone/fileio/implementation/XmlFileIO.scala
@@ -6,7 +6,7 @@ import de.htwg.se.thirtyone.model.gameImplementation.GameData
 import scala.xml._
 
 class XmlFileIO extends FileIO{
-  val file = name + ".xml"
+  val file = filepath.toString + ".xml"
   override def save(game: GameInterface): Unit =
     val xml = game.toXml()
     XML.save(file, xml)
