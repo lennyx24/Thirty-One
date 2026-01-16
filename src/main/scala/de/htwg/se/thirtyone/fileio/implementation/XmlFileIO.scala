8bd72f00930d1066f605eea381a39de9e73cd97d
lennyx24 <lenny.florin@gmail.com>
2026-01-16 12:23:43 +0100

    Fix import statement syntax in XmlFileIO for consistency

--- src/main/scala/de/htwg/se/thirtyone/fileio/implementation/XmlFileIO.scala
+++ src/main/scala/de/htwg/se/thirtyone/fileio/implementation/XmlFileIO.scala
@@ -3,7 +3,7 @@ package de.htwg.se.thirtyone.fileio.implementation
 import de.htwg.se.thirtyone.fileio.FileIO
 import de.htwg.se.thirtyone.model.GameInterface
 import de.htwg.se.thirtyone.model.gameImplementation.GameData
-import scala.xml.*
+import scala.xml._
 
 class XmlFileIO extends FileIO{
   val file = name + ".xml"
